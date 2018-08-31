abff13e7476ea5d06f2c07adc806c9f1d35e09f7
Babak Naderi <babak.naderi@tu-berlin.de>
2018-08-31 17:31:10 +0200

    support training session with show code

--- server/verification.php
+++ server/verification.php
@@ -50,6 +50,7 @@ $gameStat= getIfSet($_REQUEST["gs"],'NN');
 $istraining=getIfSet($_REQUEST["train"],0);
 $hasPlayed=getIfSet($_REQUEST["hp"],1);
 $md5=getIfSet($_REQUEST["md"],'NN');
+$showToken=getIfSet($_REQUEST["st"],0);
 
 //echo "Step 2";
 
@@ -64,13 +65,20 @@ $template='<html> <header>   <title>Finished</title>   <link href=" ../css/boots
 
 $templateTraining='<html>  <header>    <title>Training session is finished</title>    <title>Finished</title>    <link href=" ../css/bootstrap.min.css" rel="stylesheet" />    <script src=" ../js/bootstrap.min.js"></script>    <meta name="viewport" content="width=device-width, initial-scale=1">    <meta charset="utf-8">    <meta name="viewport" content="width=device-width, initial-scale=1">  </header>    <body>      <section class="container" id="Requirements" style="margin-top:20px; padding: 10px 10px;">    <div class="card">    <h5 class="card-header"><strong>Training session is finished!</strong></h5>    <div class="card-body">      <p>Good job! Now it is time to play the real games! Please go back to the MTurk website and follow the instruction there.</p>    </div>  </div>    </section>          </body>    </html>  ';
 
-$templateNotPlaying='<html>  <header>    <title>Finished</title>    <link href="../css/bootstrap.min.css" rel="stylesheet" />    <script src="../js/bootstrap.min.js"></script>      <meta charset="utf-8">    <meta name="viewport" content="width=device-width, initial-scale=1">    <script>function goBack() {    window.history.back()}</script>  </header>    <body>          <section class="container" id="Requirements" style="margin-top:20px; padding: 10px 10px;">      <div class="card">      <h5 class="card-header"><strong>Finished...</strong></h5>      <div class="card-body">        <p>During your playing we collected statistics on your performance. Those indicate that you were not well engaged in the game. This will result to rejection of your answer. So, to avoid that, we are showing this message to you and give you          another opportunity to play the game with more attention. </p>        <p>Please understand that we used this information for scientific research, and we would like to avoid you wasting your time.</p> <button type="button" class="btn btn-warning" onclick="goBack()">Try Again</button>      </div>    </div>    </section>    </body>    </html>  ';
+$tmplateTrainingShowMessage='<html> <header>   <title>Training session is finished</title>   <title>Finished</title>   <link href="  ../css/bootstrap.min.css" rel="stylesheet" />   <script src="  ../js/bootstrap.min.js"></script>   <script src="  ../js/jquery.min.js"></script>   <meta name="viewport" content="width=device-width, initial-scale=1">   <meta charset="utf-8">   <meta name="viewport" content="width=device-width, initial-scale=1">   <script>     function myFunction() {     var copyText = document.getElementById("myInput");     copyText.select();     document.execCommand("Copy");     $("#success-alert").fadeTo(2000, 500).slideUp(500, function(){     $("#success-alert").slideUp(500); });   } </script> </header> </header>  <body>   <section class="container" id="Requirements" style="margin-top:20px; padding: 10px 10px;">     <div class="card"> <h5 class="card-header"><strong>Training session is finished!</strong></h5>       <div class="card-body">         <p>Good job! Now it is time to play the real games!<p/>           <p>Now, please go back to the MTurk website and paste the code below in the corresponding box and follow the instructions there.</p>           <p><input type="text" value='."$verificationCode".' id="myInput"  size="35"> <button type="button" class="btn btn-primary" onclick="myFunction()">Copy</button>        </div>     </div>   </section>   <section class="container" id="messages" style="margin-top:20px; padding: 10px 10px;">     <div class="alert alert-success" id="success-alert" style="display:none"> <button type="button" class="close" data-dismiss="alert">x</button> <strong>Success! </strong> Text is copied. </div>   </section> </body>  </html> ';
+
+
+$templateNotPlaying='<html>  <header>    <title>Finished</title>    <link href="../css/bootstrap.min.css" rel="stylesheet" />    <script src="../js/bootstrap.min.js"></script>      <meta charset="utf-8">    <meta name="viewport" content="width=device-width, initial-scale=1">    <script>function goBack() {    window.history.back()}</script>  </header>    <body>          <section class="container" id="Requirements" style="margin-top:20px; padding: 10px 10px;">      <div class="card">      <h5 class="card-header"><strong>Finished...</strong></h5>      <div class="card-body">        <p>During your playing we collected statistics on your performance. Those indicate that you were not well engaged in the game. This will result to rejection of your answer. So, to avoid that, we are showing this message to you and give you          another opportunity to play the game with more attention. </p>        <p>Please understand that we used this information for scientific research, and we would like to avoid you wasting your time.</p> <p><b>Note:</b> When you get back to the previous page and the game is not restarting by itself, please refresh the page manually.</p> <button type="button" class="btn btn-warning" onclick="goBack()">Try Again</button>      </div>    </div>    </section>    </body>    </html>  ';
 
 if (mysqli_query($conn, $sql)) {
   if ($hasPlayed==0){
     echo $templateNotPlaying;
   }elseif ($istraining){
-    echo $templateTraining;
+    if ($showToken){
+      echo $tmplateTrainingShowMessage;
+    }else{
+      echo $templateTraining;
+    }
   }else{
     echo $template;
     }
